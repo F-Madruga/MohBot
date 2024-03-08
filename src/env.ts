da95f0c523937ee158cb1ef1a94dccf946e48251
Francisco Madruga <madruga.francisco@protonmail.com>
2024-03-08 12:37:03 +0000

    Import env variables

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,37 @@
 import { Type } from '@sinclair/typebox';
 import { Value } from '@sinclair/typebox/value';
 
-const envSchema = Type.Object({
-    NODE_ENV: Type.Union([
-        Type.Literal('test'),
-        Type.Literal('dev'),
-        Type.Literal('prod'),
-    ]),
-    LOGGER_LEVEL: Type.Union([
-        Type.Literal('fatal'),
-        Type.Literal('error'),
-        Type.Literal('warn'),
-        Type.Literal('info'),
-        Type.Literal('debug'),
-        Type.Literal('trace'),
-    ]),
+export enum NodeEnv {
+    TEST = 'test',
+    DEV = 'dev',
+    PROD = 'prod',
+}
+
+export const NodeEnvSchema = Type.Enum(NodeEnv);
+
+export enum LogLevel {
+    ERROR = 'error',
+    WARN = 'warn',
+    INFO = 'info',
+    DEBUG = 'debug',
+    FATAL = 'fatal',
+    TRACE = 'trace',
+}
+
+export const LogLevelSchema = Type.Enum(LogLevel);
+
+export const envSchema = Type.Object({
+    NODE_ENV: NodeEnvSchema,
+    LOGGER_LEVEL: LogLevelSchema,
+    DISCORD_TOKEN: Type.String(),
+    CLIENT_ID: Type.String(),
+    GUILD_ID: Type.String(),
 });
 
 export default Value.Decode(envSchema, {
     NODE_ENV: process.env.NODE_ENV,
     LOGGER_LEVEL: process.env.LOGGER_LEVEL,
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    GUILD_ID: process.env.GUILD_ID,
 });
--- src/tools/logger.ts
+++ src/tools/logger.ts
@@ -1,17 +1,5 @@
 import pino from 'pino';
-import { Type } from '@sinclair/typebox';
 import env from '../env';
-
-export enum LogLevel {
-    ERROR = 'error',
-    WARN = 'warn',
-    INFO = 'info',
-    DEBUG = 'debug',
-    FATAL = 'fatal',
-    TRACE = 'trace',
-}
-
-export const LogLevelSchema = Type.Enum(LogLevel);
 
 export const logger = pino({
     name: 'MohBot',
