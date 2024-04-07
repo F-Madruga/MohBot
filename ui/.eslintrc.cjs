ea834b4bc053b8c87fb5da2d946740f6207e1602
Francisco Madruga <madruga.francisco@protonmail.com>
2024-04-07 23:42:51 +0100

    Setup UI

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:import/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
         'plugin:import/typescript',
