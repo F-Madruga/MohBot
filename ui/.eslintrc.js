89b95ed025848e023e3c2acfb84ff89ca90d81cd
Francisco Madruga <madruga.francisco@protonmail.com>
2024-04-07 23:22:17 +0100

    Setup UI

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,31 +1,24 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:import/recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        'prettier',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: '.',
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    plugins: ['@typescript-eslint', 'import', 'prettier'],
-    rules: {
-        'indent': ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always'],
-        'prettier/prettier': 'error',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': ['error'],
-        '@typescript-eslint/no-explicit-any': 'warn',
-    },
-};
+  },
+}
